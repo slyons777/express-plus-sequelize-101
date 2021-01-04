3611936433a4765d4517b4091cc51e0fdb0497c5
Sarina Lyons <sarinalyons@gmail.com>
2021-01-04 15:27:37 -0500

    first few changes

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const logger = morgan('tiny');
 // JS library that speaks to Postgres
 const Sequelize = require('sequelize');
 // Pets is an object that can
-const { Pets } = require('./models');
+const {
+  Pets
+} = require('./models');
 
 const db = [];
 
@@ -23,14 +25,16 @@ app.use(logger);
 // app.use(helmet());
 
 // Parse any form data from POST requests
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Hello!</h1><br><a href="/new">Go to the form</a>`)
+  res.send(`<h1>Hello!</h1><br><a href="/new">Go to the form</a>`)
 });
 
 app.get('/new', (req, res) => {
-    res.send(`
+  res.send(`
 <h1>Say something!</h1>
 <form method="POST">
   <label>
@@ -50,34 +54,37 @@ app.get('/new', (req, res) => {
 // Put async in front of (req, res)
 // It means that you will use the `await` keyword in the function
 app.post('/new', async (req, res) => {
-    const { name, breed } = req.body;
-    // db.push(thought);
-    // await will "pause" before running the rest
-    // of the function.
-    // 1. We start to talk to Postgres: Pets.create()
-    // 2. Pause...until Postgres answers
-    // 3. Whenever we finish talking to Postgres, assign result
-    //    to new variable: newPet
-    // const petDataFromTheForm = {
-    //     name,   // equivalent to name: name
-    //     breed   // equivalent to breed: breed
-    // };
-    // const newPet = await Pets.create(petDataFromTheForm);
-    // console.log(newPet);
-    await Pets.create({
-      name,
-      breed
+  const {
+    name,
+    breed
+  } = req.body;
+  // db.push(thought);
+  // await will "pause" before running the rest
+  // of the function.
+  // 1. We start to talk to Postgres: Pets.create()
+  // 2. Pause...until Postgres answers
+  // 3. Whenever we finish talking to Postgres, assign result
+  //    to new variable: newPet
+  // const petDataFromTheForm = {
+  //     name,   // equivalent to name: name
+  //     breed   // equivalent to breed: breed
+  // };
+  // const newPet = await Pets.create(petDataFromTheForm);
+  // console.log(newPet);
+  await Pets.create({
+    name,
+    breed
   });
-    res.redirect('/list');    
+  res.redirect('/list');
 });
 
 app.get('/list', (req, res) => {
   // Read/retrieve all pets
-    Pets.findAll()
-        .then(pets => {
-            // res.json(pets);
-            //console.log(pets);
-              res.send(`
+  Pets.findAll()
+    .then(pets => {
+      // res.json(pets);
+      //console.log(pets);
+      res.send(`
           <a href="/new">Go to the form</a>
           <ul>
             ${
@@ -85,9 +92,81 @@ app.get('/list', (req, res) => {
             }
           </ul>
               `);
-        })
+    })
 });
 
+//updating
+app.get('/list/:id', async (req, res) => {
+  // updating list of all pets
+  Pets.findAll()
+    .then(pets => {
+      // res.json(pets);
+      //console.log(pets);
+      res.send(`
+          <a href="/new">Go to the form</a>
+          <ul>
+            ${
+              pets.map(pet => `<li>${pet.name}: ${pet.breed}</li>`).join('')
+            }
+          </ul>
+              `);
+    })
+});
+
+app.post('/list/:id'
+  async, (req, res) => {
+    // returning update of new list
+    Pets.findAll()
+      .then(pets => {
+        // res.json(pets);
+        //console.log(pets);
+        res.send(`
+          <a href="/new">Go to the form</a>
+          <ul>
+            ${
+              pets.map(pet => `<li>${pet.name}: ${pet.breed}</li>`).join('')
+            }
+          </ul>
+              `);
+      })
+  });
+
+//deleting
+app.get('/list/:id/delete', (req, res) => {
+  // find pet to delete
+  Pets.findAll()
+    .then(pets => {
+      // res.json(pets);
+      //console.log(pets);
+      res.send(`
+          <a href="/new">Go to the form</a>
+          <ul>
+            ${
+              pets.map(pet => `<li>${pet.name}: ${pet.breed}</li>`).join('')
+            }
+          </ul>
+              `);
+    })
+});
+
+app.post('/list/:id/delete', (req, res) => {
+  // remove specific pet
+  Pets.findAll()
+    .then(pets => {
+      // res.json(pets);
+      //console.log(pets);
+      res.send(`
+          <a href="/new">Go to the form</a>
+          <ul>
+            ${
+              pets.map(pet => `<li>${pet.name}: ${pet.breed}</li>`).join('')
+            }
+          </ul>
+              `);
+    })
+});
+
+
 server.listen(PORT, HOST, () => {
-    console.log(`Listening at http://${HOST}:${PORT}`);
+  console.log(`Listening at http://${HOST}:${PORT}`);
 });
